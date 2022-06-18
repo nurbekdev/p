e0b46a1a2c20f9c0dcfd58cca44e5de9343de015
Nurbek Po'latov <polatovnurbek622@gmail.com>
2022-06-18 13:22:26 +0500

    aaa

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -48,7 +48,7 @@ export const Navigation = ({ isOpen, toggleOpen }: any) => {
             <span className="brand-logo__text-wrapper">
               <img
                 className="brand-logo__text"
-                src="svg/"
+                src="svg/adeola-logo-right.webp"
                 alt="Logo yosh maslahatchilar"
               />
             </span>
